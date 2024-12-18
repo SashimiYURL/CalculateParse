769776ef449e6ebf5a68af364fa1dc2dd0883d0c
Admin <kkkkkkp_16@mail.ru>
2024-12-18 20:17:38 +0300

    Добавила базовый тест для двух переменных

--- TestCalculate/BasicTests.cs
+++ TestCalculate/BasicTests.cs
@@ -3,10 +3,15 @@ namespace TestCalculate
 {
     public class BasicTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("2 + 2", "4")]
+        [InlineData("2 - 2", "0")]
+        [InlineData("2 / 2", "1")]
+        [InlineData("2 * 2", "4")]
+        public void Calculate_OneOperationWithTwoNumbers(string expression, string result)
         {
-            
+            var calculator = new Calculator(expression);
+            Assert.Equal(result, calculator.Calculate());
         }
     }
 }
